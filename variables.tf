fd0149ca9575a9ba5220e8575df23e0e9d57af3b
BD <mrdobbs2010@gmail.com>
2024-11-19 23:59:54 -0600

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,13 @@
+﻿variable "aws_region" {
+	description = "The AWS region in which to create resources."
+	default = "us-east-2"
+}
+
+variable "instance_type" {
+	description = "The instance type of the EC2 instance."
+	default = "t2.micro"
+}
+
+variable "ami_id" {
+	description = "The AMI ID to use for the EC2 instance."
+}
---
+++ versions.tf
@@ -0,0 +1,10 @@
+﻿terraform {
+	required_version = ">= 0.12"
+
+	required_providers {
+		aws = {
+		source = "hashicorp/aws"
+		version = "~> 3.0"
+		}
+	}
+}
