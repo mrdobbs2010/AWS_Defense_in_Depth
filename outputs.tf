a645be1d43e35162bc82024f218ed503b9faf341
BD <mrdobbs2010@gmail.com>
2024-11-23 23:46:09 -0600

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-﻿output "vpc_id" {
-	description = "The ID of the VPC."
-	value = aws_vpc.squatch_vpc.id
-}
-
-output "instance_public_ip" {
-	description = "The public IP of the EC2 instance."
-	value = aws_instance.sample_instance.public_ip
-}
